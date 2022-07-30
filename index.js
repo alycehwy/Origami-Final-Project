20334903ee231633189b999565699de202966e12
alycehwy <pary2451@gmail.com>
2022-07-30 15:37:57 -0700

    move script to js file

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+let spaApp = angular.module("spaApp",["ngRoute"]);
+spaApp.config(($routeProvider)=>{
+    $routeProvider
+    .when("/",{
+        templateUrl:"./pages/home.html"
+    })
+    .when("/menu",{
+        templateUrl:"./pages/menu.html"
+    })
+    .when("/cart",{
+        templateUrl:"./pages/cart.html"
+    })
+    .when("/payment",{
+        templateUrl:"./pages/pay.html"
+    })
+})
+spaApp.run(($rootScope)=>{
+    $rootScope.loginPage = true;
+    $rootScope.requiredName = false;
+    $rootScope.requiredLoc = false;
+})
+spaApp.controller("spaCtrl",($scope)=>{
+    $scope.loginPageClose = ()=>{
+        if($scope.username == undefined && $scope.location == undefined){
+            $scope.requiredName = true;
+            $scope.requiredLoc = true;
+        }
+        else if($scope.username == undefined || $scope.username == ""){
+            $scope.requiredName = true;
+        }
+        else if($scope.location == undefined || $scope.location == ""){
+            $scope.requiredLoc = true;
+        }
+        else{
+            $scope.loginPage = false;
+        }
+    };
+    $scope.payClick = ()=>{
+        this.router.navigate("/payment");
+    }
+})
+
 // Modal
 var modal = document.getElementById("cart-modal");
 var btn = document.getElementById("myBtn");
